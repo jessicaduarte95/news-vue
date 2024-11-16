9f982d8728c1d30a0f2d7713d0829d96ea38a65c
Jessica Duarte <jessicaduarte.95@gmail.com>
2024-11-16 15:46:46 -0300

    feat: add cards in onboarding

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -20,6 +20,5 @@ export default {
 <style scoped>
 .app-main {
 	width: 100%;
-	height: 100%;
 }
 </style>
--- src/views/OnboardingView.vue
+++ src/views/OnboardingView.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="container-teste"></div>
+  <div class="container-onboarding">
+    <div class="cards">
+      <div class="card-onboarding"></div>
+      <div class="card-onboarding"></div>
+    </div>
+    <div class="cards">
+      <div class="card-onboarding"></div>
+      <div class="card-onboarding"></div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,12 +18,48 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.container-teste {
+@import "../assets/styles/variables";
+
+.container-onboarding {
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: center;
   height: 100%;
   width: 100%;
   padding-top: 1.5rem;
+  background-color: $grayColor;
+  gap: 5rem;
+}
+
+.cards {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 5rem;
+}
+
+.card-onboarding {
+  display: flex;
+  flex-direction: row;
+  background-color: $whiteColor;
+  border-radius: 1rem;
+  box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.15);
+  width: 40%;
+  height: 20rem;
+}
+
+.mobile {
+  .container-onboarding {
+    padding: 3.5rem 0rem 3.5rem 0rem;
+    gap: 2.5rem;
+  }
+  .cards {
+    flex-direction: column;
+    align-items: center;
+    gap: 2.5rem;
+  }
+  .card-onboarding{
+    width: 20rem;
+  }
 }
 </style>
